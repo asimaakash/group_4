1b831afab4096e354461168291c2a5382e5bafa3
Asim Aakash <aakashasim@gmail.com>
2020-08-16 15:14:40 +0530

    Web Page

---
+++ practice/sample.js
@@ -0,0 +1,5 @@
+let x = document.getElementById("fname");
+function f() {
+  document.getElementById("output").innerHTML = "You Entered <br />";
+  document.getElementById("output").innerHTML += x.value;
+}
