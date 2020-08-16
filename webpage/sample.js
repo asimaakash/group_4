f6da9ca171503e7e4495a247aa1a42821381497d
Asim Aakash <aakashasim@gmail.com>
2020-08-16 16:37:46 +0530

    Styling
    styling on message me

--- webpage/sample.js
+++ webpage/sample.js
@@ -1,5 +1,9 @@
 let x = document.getElementById("fname");
+let y = document.getElementById("profession");
+let z = document.getElementById("email");
 function f() {
   document.getElementById("output").innerHTML = "You Entered <br />";
-  document.getElementById("output").innerHTML += x.value;
+  document.getElementById("output").innerHTML += "<p>" + x.value + "</p>";
+  document.getElementById("output").innerHTML += "<p>" + y.value + "</p>";
+  document.getElementById("output").innerHTML += "<p>" + z.value + "</p>";
 }
